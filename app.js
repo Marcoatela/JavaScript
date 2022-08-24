23b581c44ba668e113261960bfd8bffc0afd5f59
Marco Atela <atelamarco@gmail.com>
2022-08-23 23:45:57 -0300

    While Saludo

---
+++ app.js
@@ -0,0 +1,16 @@
+let i = 0;
+
+
+while(saludo != "Hola" && saludo != "hola"){
+    var saludo = prompt("Salude para ingresar");
+    i++;
+}
+
+alert("Buenos dias");
+
+/* while (nombre != "ESC" && nombre != "esc" ) {
+    saludo  = nombre + " ";
+    nombre = prompt("Ingrese Nombr o ESC para interrumpir");
+}
+
+alert(texto); */
