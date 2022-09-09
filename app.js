9da4c657615fffbcdb11d3d271a9a0b270f40604
Marco Atela <atelamarco@gmail.com>
2022-09-08 23:38:28 -0300

    pre entrega 1

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-const vino = {
-    id: 1,
-    bodega: "Luigi Bosca",
-    cosecha: 2010,
-    precio: 1750,
-    variedad: "Malbec",
-    disponible : true,
-    oferta:["Descuento 15%", "Envio gratis"],
-};
 
-const {bodega, precio, variedad, oferta} = vino;
 
-console.log(bodega, variedad, precio, oferta[1]);
+const vinos = [
+    {nombre: "Pispi", bodega: "Mosquita Muerta", variedad: "Malbec", año: 2010, precio: 1600},
+    {nombre: "Coleccion Rutini", bodega: "Rutini", variedad: "Malbec", año: 2018, precio: 2700},
+    {nombre: "DvCatena", bodega: "Catena Zapata", variedad: "Cabernet", año: 2009, precio: 5900},
+    {nombre: "El enemigo", bodega: "Aleanna", variedad: "Malbec", año: 2020, precio: 4860},
+    {nombre: "Solito Va", bodega: "El cepillo", variedad: "Cabernet Franc", año: 2022, precio: 1140},
+
+]
+
+let nombresVinos = vinos.map(function(vino){
+    return vino.nombre;
+    
+})
+
+vinoSelect = prompt(`Elegi un vino de la siguiente lista: ${nombresVinos}`);
+
+function precioVino(){
+    for (let vino of vinos){
+        if(vino.nombre == vinoSelect){
+            if(vino.precio >= 3000){
+                alert(`El vino seleccionado tiene envío gratis, porque cuesta mas de $2999`)
+            } else{
+                alert(`El costo del envío es de $800`)
+                
+            }
+        }
+    }
+}
+
+
+precioVino();
